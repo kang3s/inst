fa847223aae9d660c9749c47bdda8944d0e0fdab
kang3s <100813691+kang3s@users.noreply.github.com>
2025-05-16 08:20:14 +0700

    Update longrunpipebgcmd_redirectermoniter.sh

--- _build/inst/localinstall-patchs/amd64/longrunpipebgcmd_redirectermoniter.sh
+++ _build/inst/localinstall-patchs/amd64/longrunpipebgcmd_redirectermoniter.sh
@@ -27,76 +27,63 @@ for step in pass1 pass2 pass3; do
     fi
 
     case $step in
-        "pass1")
-            db_progress START 0 100 my_script/progress/pass1
-            db_progress INFO my_script/progress/pass1
-            db_progress SET 0
-        
-            mkdir -p /osmnt
-            if ! mountpoint -q "/osmnt"; then
-                mount -t ext4 "$srchdinfo$srcptinfo" /osmnt || { echo "Mount failed"; exit 1; }
-            fi
-        
-            filesize=$(wc -c < "/osmnt/$filepathinfo/$filenameinfo")
+       "pass1")
+           db_progress START 0 100 my_script/progress/pass1
+           db_progress INFO my_script/progress/pass1
+           db_progress SET 0
+
+            mkdir -p /osmnt;if mountpoint -q "/osmnt";then :;else mount -t ext4 "$srchdinfo$srcptinfo" /osmnt; fi
+
+            filesize=$(wc -c < /osmnt/"$filepathinfo"/"$filenameinfo")
             disksize=$(lsblk -b -nr -o SIZE "$srchdinfo" -d)
             ptsize=$(lsblk -b -nr -o SIZE "$srchdinfo$srcptinfo")
-            PARTITION_COUNT=$(lsblk -nr -o NAME,TYPE "$srchdinfo" | awk '$2 == "part" {print $1}' | wc -l)
-        
-            if [ "$PARTITION_COUNT" -gt "$srcptinfo" ]; then
-                for i in $(seq $((srcptinfo + 1)) $PARTITION_COUNT); do
-                    parted -s "$srchdinfo" rm "$i"
-                done
+            PARTITION_COUNT=$(lsblk -nr -o NAME,TYPE $srchdinfo | awk '$2 == "part" {print $1}'| wc -l)
+            if [ $PARTITION_COUNT -gt $srcptinfo ]; then
+              for i in `seq $((srcptinfo + 1)) $PARTITION_COUNT`;do
+                parted -s $srchdinfo rm $i
+              done
             fi
-        
-            diskavaliable=$(parted -s "$srchdinfo" unit b print free | awk '/Free Space$/ {end=$3} END{print end}' | sed 's/B$//')
-        
-            # Ambil nilai terbesar antara Used dan Available kolom dari df (kolom 3 dan 4)
-            read used available <<< $(df -B1 | grep "$srchdinfo$srcptinfo" | awk '{print $3, $4}')
-            if [ "$used" -gt "$available" ]; then
-                partavaliable=$used
-            else
-                partavaliable=$available
-            fi
-        
+            diskavaliable=$(parted -s $srchdinfo unit b print free | awk '/Free Space$/ {end=$3} END{print end}' | sed 's/B$//')
+            #find /osmnt -mindepth 1 ! -path /osmnt/"$filepathinfo"/1.gz ! -path /osmnt/"$filepathinfo" ! -path /osmnt/"$filepathinfo"/* -exec rm -rf {} +
+            partavaliable=$(df -B1 | grep "$srchdinfo$srcptinfo" | awk '{print ($3 > $4 ? $3 : $4)}')
             logger -t pass1 filesize:$filesize byte,disksize:$disksize byte,ptsize:$ptsize byte,diskavaliable:$diskavaliable byte,partavaliable:$partavaliable byte
-        
-            if [ "$diskavaliable" -ge "$filesize" ]; then
-                logger -t pass1 enough
-                PIPECMDSTR1='dd if=/osmnt/'$filepathinfo'/'$filenameinfo' of='$hdinfo' bs=512 seek=$((('$disksize' - '$filesize') / 512 )) conv=notrunc 2>> /var/log/progress1 & pid=`expr $! + 0`;echo $pid'
-            else
-                needborrowsize=$((filesize - diskavaliable))
-                borrowoffset=$((ptsize - needborrowsize))
-                logger -t pass1 not enough,need borrow from part:$needborrowsize byte,borrowoffset:$borrowoffset
-        
-                if [ "$partavaliable" -ge "$needborrowsize" ]; then
-                    printf "unit\nb\nresizepart\n$srcptinfo\nYes\n$borrowoffset\nYes\nquit" | parted "$srchdinfo" ---pretend-input-tty >/dev/null 2>&1
-                    logger -t pass1 enough
-                    PIPECMDSTR1='dd if=/osmnt/'$filepathinfo'/'$filenameinfo' of='$hdinfo' bs=512 seek=$((('$disksize' - '$filesize') / 512 )) conv=notrunc 2>> /var/log/progress1 & pid=`expr $! + 0`;echo $pid'
-                else
-                    logger -t pass1 hardly happen
-                    exit 1
-                fi
-            fi
-        
-            logger -t minlearnadd preddtime PIPECMDSTR1:"$PIPECMDSTR1"
-            pidinfo=$(eval "$PIPECMDSTR1")
-        
-            PCTCOUNT=0
-            while :; do
-                sleep 3
-                statusinfo=$(kill -USR1 "$pidinfo"; cat /var/log/progress1 | sed '/^$/!h;$!d;g')
-                db_subst my_script/progress/pass1 STATUS "${statusinfo}"
-                db_progress STEP 1
-                if kill -s 0 "$pidinfo"; then :; else db_progress SET 100; break; fi
-            done
-        
-            sleep 3
-            umount -f /osmnt
-            if mountpoint -q "/osmnt"; then
-                echo "still mounted" && exit 1
-            fi
-            ;;
 
+            if [ $diskavaliable -ge $filesize ]; then
+              logger -t pass1 enough
+              PIPECMDSTR1='dd if=/osmnt/'$filepathinfo'/'$filenameinfo' of='$hdinfo' bs=512 seek=$((('$disksize' - '$filesize') / 512 )) conv=notrunc 2>> /var/log/progress1 & pid=`expr $! + 0`;echo $pid'
+            else
+              needborrowsize=$(($filesize - $diskavaliable))
+              borrowoffset=$(($ptsize - $needborrowsize))
+              logger -t pass1 not enough,need borrow from part:$needborrowsize byte,borrowoffset:$borrowoffset
+
+              if [ $partavaliable -ge $needborrowsize ]; then
+                printf "unit\nb\nresizepart\n$srcptinfo\nYes\n$borrowoffset\nYes\nquit" | parted "$srchdinfo" ---pretend-input-tty >/dev/null 2>&1
+                logger -t pass1 enough
+                PIPECMDSTR1='dd if=/osmnt/'$filepathinfo'/'$filenameinfo' of='$hdinfo' bs=512 seek=$((('$disksize' - '$filesize') / 512 )) conv=notrunc 2>> /var/log/progress1 & pid=`expr $! + 0`;echo $pid'                          
+              else
+                logger -t pass1 hardly happen
+                exit 1
+              fi
+            fi
+
+            logger -t minlearnadd preddtime PIPECMDSTR1:"$PIPECMDSTR1"
+            pidinfo=`eval "$PIPECMDSTR1"`
+            PCTCOUNT="0";
+            while :; do 
+            {
+              sleep 3
+              statusinfo=`kill -USR1 $pidinfo;cat /var/log/progress1|sed '/^$/!h;$!d;g'`
+              db_subst my_script/progress/pass1 STATUS "${statusinfo}"
+              tillnowinfo=`echo $statusinfo|sed 's/bytes \(.*\)//g'`
+              db_progress STEP 1
+            }
+            if kill -s 0 $pidinfo; then :; else { db_progress SET 100 && break; }; fi
+            done
+            sleep 3
+
+            umount -f /osmnt;if mountpoint -q "/osmnt";then echo "still mounted" && exit 1; fi
+
+           ;;
 
        "pass2")
            db_progress START 0 100 my_script/progress/pass2
